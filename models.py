2c850f63edb294543927ce290100e3398fe73a6b
PS <ps@objectsource.de>
2023-03-24 05:00:21 +0100

    extended schema model

--- models.py
+++ models.py
@@ -17,11 +17,42 @@ Base = declarative_base()
 # We will need this for querying
 Base.query = db_session.query_property()
 
+class Bank(Base):
+    __tablename__ = 'res_bank'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    state = Column(String)
+    country = Column(String)
+    street = Column(String)
+    street2 = Column(String)
+    zip = Column(String)
+    city = Column(String)
+    email = Column(String)
+    phone = Column(String)
+    bic = Column(String)
+
+class Company(Base):
+    __tablename__ = 'res_company'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
 
 class Partner(Base):
     __tablename__ = 'res_partner'
     id = Column(Integer, primary_key=True)
     name = Column(String)
+    title = Column(String)
+    lang = Column(String)
+    website = Column(String)
+    function = Column(String)
+    type = Column(String)
+    street = Column(String)
+    street2 = Column(String)
+    zip = Column(String)
+    city = Column(String)
+    email = Column(String)
+    phone = Column(String)
+    mobile = Column(String)
+    company_name = Column(String)
 
 class User(Base):
     __tablename__ = 'res_users'
--- schema.py
+++ schema.py
@@ -1,7 +1,26 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
-from models import db_session, Partner as PartnerModel, User as UserModel
+from models import db_session, Bank as BankModel, Company as CompanyModel, Partner as PartnerModel, User as UserModel
+
+class Bank(SQLAlchemyObjectType):
+    class Meta:
+        model = BankModel
+        interfaces = (relay.Node, )
+
+
+class BankConnection(relay.Connection):
+    class Meta:
+        node = Bank
+
+class Company(SQLAlchemyObjectType):
+    class Meta:
+        model = CompanyModel
+        interfaces = (relay.Node, )
+
+class CompanyConnection(relay.Connection):
+    class Meta:
+        node = Company
 
 class Partner(SQLAlchemyObjectType):
     class Meta:
@@ -27,6 +46,8 @@ class UserConnection(relay.Connection):
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
     all_users = SQLAlchemyConnectionField(User._meta.connection)
+    all_banks = SQLAlchemyConnectionField(Bank._meta.connection)
+    all_companies = SQLAlchemyConnectionField(Company._meta.connection)
     all_partners = SQLAlchemyConnectionField(Partner._meta.connection)
 
 schema = graphene.Schema(query=Query)
